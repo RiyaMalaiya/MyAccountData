5b4d7616e7249516481faf3c1de5080bb96513cc
RiyaMalaiya <riyamalaiyaengeng@gmail.com>
2022-12-24 23:20:24 +0530

    commit

---
+++ src/main/java/com/model/AccountData.java
@@ -0,0 +1,68 @@
+package com.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import lombok.Data;
+
+@Data
+@DynamoDBTable(tableName = "AccountData")
+public class AccountData {
+
+	@DynamoDBHashKey
+	String profileId;
+
+	@DynamoDBAttribute
+	int accountId;
+	
+	@DynamoDBAttribute
+	String accountName;
+	
+	@DynamoDBAttribute
+	String address;
+	
+	@DynamoDBAttribute
+	String accountType;
+
+	public String getProfileId() {
+		return profileId;
+	}
+
+	public void setProfileId(String profileId) {
+		this.profileId = profileId;
+	}
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+}
